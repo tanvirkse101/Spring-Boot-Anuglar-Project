7e10c0c1311f643246ce3bebdc6172cb9295988c
RaihanSabique <htraihan@gmail.com>
2020-12-20 20:19:03 +0600

    Angular tutorial + mongodb Spring API operation

--- spring-boot-rest-api/src/main/java/com/example/restservice/controllers/tutorial/TutorialController.java
+++ spring-boot-rest-api/src/main/java/com/example/restservice/controllers/tutorial/TutorialController.java
@@ -23,6 +23,7 @@ import com.example.restservice.repository.TutorialRepository;
 
 //@CrossOrigin(origins = "http://localhost:8081")
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class TutorialController {
 
@@ -91,7 +92,7 @@ public class TutorialController {
     @PostMapping("/tutorials")
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
         try {
-            Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), tutorial.getPublished()));
             return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
